08e00759843ef891f8dc29c651b41d583995f674
Abenezer Seifu <57215372+abenezer121@users.noreply.github.com>
2022-09-13 11:12:21 +0300

    adding geocoding

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# gebetanodesdk
+
+A composable, Future-based library for making HTTP requests to GebetaMap.
+
+This package contains a set of high-level functions that make it
+easy to consume HTTP resources from GebetaMap. It's multi-platform, and supports mobile, desktop,
+and the browser.
+
+## Using
+making direction request to gebeta map
+```js
+    import { direction } from 'gebetamap';
+    let start = {lat : 9.4343 , lon : 38.,434534}
+    let stop = {lat :9.2334 , lon : 38.53432}
+    const apiKey = ""
+    let data = await direction(start , stop , apiKey) 
+
+```
+
+making travels salesman request to gebeta map
+
+```js
+import { tss } from 'gebetamap';
+
+  const  apiKey = "";
+  let points = [
+    {lat: 9.021739361296081, lng: 38.80397726479262},
+    {lat: 9.02153803517439, lng: 38.7967248502125},
+    {lat: 9.022671817658015, lng: 38.800082846904175},
+    {lat: 9.023773808433624, lng: 38.7980015512}]
+    const ts = await tss(points , apiKey)
+
+ 
+```
+
+making one to many request 
+```js
+import { oneToMany } from 'gebetamap';
+   let start = {lat : 9.4343 , lon : 38.,434534}
+   const apiKey = ""
+   let points = [
+    {lat: 9.021739361296081, lng: 38.80397726479262},
+    {lat: 9.02153803517439, lng: 38.7967248502125},
+    {lat: 9.022671817658015, lng: 38.800082846904175},
+    {lat: 9.023773808433624, lng: 38.7980015512}]
+    const onm = await oneToMany  (start, points, apiKey)
+
+```
+
+
+
+making Geocoding request
+``` js
+    import { geocoding } from 'gebetamap';
+    String name = "kotebe 02";
+    String apiKey = "";
+    const geo = await geocoding(name, apiKey)
+```
+
+
---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+const direction =  async (start , newMarker , apiKey) => {
+    const url = "https://mapapi.gebeta.app/api/v1/route/driving/direction/?la1=" + start.lat + "&lo1=" + start.lon + "&la2=" + newMarker.lat + "&lo2=" + newMarker.lng + "&apiKey="+apiKey
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+      } catch (err) {
+        return err;
+      }
+}
+
+const matrix = async (gmarker, apiKey) => {
+
+  let startpoint = []
+    for (let i = 0; i < gmarker.length; i++){
+      let en = gmarker[i].lat +"/"+ gmarker[i].lng;
+      startpoint.push(en)
+    }
+  
+    const url = "https://mapapi.gebeta.app/api/v1/route/driving/matrix/?start=" + startpoint + "&apiKey="+apiKey
+
+  try {
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+      } catch (err) {
+        return err;
+      }
+}
+
+
+
+const tss = async (gmarker, apiKey) => {
+  let startpoint = []
+    for (let i = 0; i < points.length; i++){
+      let en = points[i].lat +"/"+ gmarker[i].lng;
+      startpoint.push(en)
+    }
+
+  const url = "https://mapapi.gebeta.app/api/v1/route/driving/tss/?start=" + startpoint + "&apiKey=" + apiKey   
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+      } catch (err) {
+        return err;
+      }
+}
+
+const oneToMany = async (start, endpoints, apiKey) => {
+  let _endpoints = []
+  for (let i = 0; i < endpoints.length; i++){
+      let en = endpoints[i].lat +"/"+ endpoints[i].lng;
+      _endpoints.push(en)
+  }
+  const url = "https://mapapi.gebeta.app/api/v1/route/driving/onm/?la1=" + start.lat + "&lo1=" + start.lon + "&json=" + _endpoints + "&apiKey=" + apiKey
+  try {
+        const response = await fetch(url);
+        const json = await response.json();
+    return json
+  } catch (error) {
+    return err;
+  }
+}
+
+const geocoding = async (name, apiKey) => {
+  const url = "https://mapapi.gebeta.app/api/v1/route/geocoding?name=" + name + "&apiKey=" + apiKey;
+  try {
+      const response = await fetch(url);
+        const json = await response.json();
+  } catch (error) {
+    return error
+  }
+
+}
+
+
+module.exports = { direction: direction,  matrix: matrix,  oneToMany: oneToMany,  tss: tss,  geocoding: geocoding }
+
