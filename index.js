6817e8ee91e01e01d08ffcc59427bffe04060e87
Abenezer Seifu <57215372+abenezer121@users.noreply.github.com>
2022-09-27 13:47:59 +0300

    return type changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 const direction =  async (start , newMarker , apiKey) => {
-    const url = "https://mapapi.gebeta.app/api/v1/route/driving/direction/?la1=" + start.lat + "&lo1=" + start.lon + "&la2=" + newMarker.lat + "&lo2=" + newMarker.lng + "&apiKey="+apiKey
+    const url = "https://gmapapi.gebeta.app/api/v1/route/driving/direction/?la1=" + start.lat + "&lo1=" + start.lon + "&la2=" + newMarker.lat + "&lo2=" + newMarker.lng + "&apiKey="+apiKey
     try {
         const response = await fetch(url);
         const json = await response.json();
@@ -13,13 +13,18 @@ const direction =  async (start , newMarker , apiKey) => {
 
 const matrix = async (gmarker, apiKey) => {
 
-  let startpoint = []
-    for (let i = 0; i < gmarker.length; i++){
-      let en = gmarker[i].lat +"/"+ gmarker[i].lng;
-      startpoint.push(en)
+
+   let startpoint = []
+  for (let i = 0; i < gmarker.length; i++){
+    let points = []
+    points.push(gmarker[i].lat);
+    points.push(gmarker[i].lng);
+
+      startpoint.push(points)
     }
   
-    const url = "https://mapapi.gebeta.app/api/v1/route/driving/matrix/?start=" + startpoint + "&apiKey="+apiKey
+  
+    const url = "https://gmapapi.gebeta.app/api/v1/route/driving/matrix/?start=" + startpoint + "&apiKey="+apiKey
 
   try {
         const response = await fetch(url);
@@ -34,12 +39,15 @@ const matrix = async (gmarker, apiKey) => {
 
 const tss = async (gmarker, apiKey) => {
   let startpoint = []
-    for (let i = 0; i < gmarker.length; i++){
-      let en = gmarker[i].lat +"/"+ gmarker[i].lng;
-      startpoint.push(en)
+  for (let i = 0; i < gmarker.length; i++){
+    let points = []
+    points.push(gmarker[i].lat);
+    points.push(gmarker[i].lng);
+
+      startpoint.push(points)
     }
 
-  const url = "https://mapapi.gebeta.app/api/v1/route/driving/tss/?start=" + startpoint + "&apiKey=" + apiKey   
+  const url = "https://gmapapi.gebeta.app/api/v1/route/driving/tss/?start=" + startpoint + "&apiKey=" + apiKey   
     try {
         const response = await fetch(url);
         const json = await response.json();
@@ -49,25 +57,28 @@ const tss = async (gmarker, apiKey) => {
       }
 }
 
-const oneToMany = async (start, endpoints, apiKey) => {
-  let _endpoints = []
-  for (let i = 0; i < endpoints.length; i++){
-      let en = endpoints[i].lat +"/"+ endpoints[i].lng;
-      _endpoints.push(en)
-  }
-  const url = "https://mapapi.gebeta.app/api/v1/route/driving/onm/?la1=" + start.lat + "&lo1=" + start.lon + "&json=" + _endpoints + "&apiKey=" + apiKey
+const oneToMany = async (start, gmarker, apiKey) => {
+ let startpoint = []
+  for (let i = 0; i < gmarker.length; i++){
+    let points = []
+    points.push(gmarker[i].lat);
+    points.push(gmarker[i].lng);
+
+      startpoint.push(points)
+    }
+  const url = "https://gmapapi.gebeta.app/api/v1/route/driving/onm/?la1=" + start.lat + "&lo1=" + start.lon + "&json=" + startpoint + "&apiKey=" + apiKey
   try {
         const response = await fetch(url);
     const json = await response.json();
     return json
-    return json
+
   } catch (error) {
-    return err;
+    return error;
   }
 }
 
 const geocoding = async (name, apiKey) => {
-  const url = "https://mapapi.gebeta.app/api/v1/route/geocoding?name=" + name + "&apiKey=" + apiKey;
+  const url = "https://gmapapi.gebeta.app/api/v1/route/geocoding?name=" + name + "&apiKey=" + apiKey;
   try {
       const response = await fetch(url);
         const json = await response.json();
